38008eee7c45da0d88ff7ad768d1b8db9011faab
Guilherme Sampaio <guilhermefs09@protonmail.com>
2025-08-24 13:52:28 -0300

    update: db port type

--- source/database/base.py
+++ source/database/base.py
@@ -5,7 +5,7 @@ import os
 db_user = os.getenv("DATABASE_USER", "user")
 db_password = os.getenv("DATABASE_PASSWORD", "password")
 db_host = os.getenv("DATABASE_HOST", "localhost")
-db_port = os.getenv("DATABASE_PORT", "5432")
+db_port = os.getenv("DATABASE_PORT", 5432)
 db_name = os.getenv("DATABASE_NAME", "database")
 
 DATABASE_URL = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
